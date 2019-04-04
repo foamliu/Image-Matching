3d544da83139fed3c73a25bd368352f9407888c1
Yang Liu <liuyang12@focusmedia.cn>
2019-04-04 14:57:45 +0800

    udpate

---
+++ data_gen.py
@@ -0,0 +1,50 @@
+import os
+import pickle
+
+import cv2 as cv
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+from config import IMG_DIR, image_w, image_h, pickle_file
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]),
+    'val': transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+
+class FrameDataset(Dataset):
+    def __init__(self, split, mode):
+        with open(pickle_file, 'rb') as file:
+            data = pickle.load(file)
+
+        self.split = split
+        self.samples = data
+        self.transformer = data_transforms[mode]
+
+    def __getitem__(self, i):
+        sample = self.samples[i]
+        filename = sample['img']
+        label = sample['label']
+
+        filename = os.path.join(IMG_DIR, filename)
+        img = cv.imread(filename)  # BGR
+        img = cv.resize(img, (image_w, image_h))
+        img = img[..., ::-1]  # RGB
+        img = Image.fromarray(img, 'RGB')  # RGB
+        img = self.transformer(img)  # RGB
+
+        return img, label
+
+    def __len__(self):
+        return len(self.samples)
--- pre_process.py
+++ pre_process.py
@@ -1,19 +1,22 @@
 import os
+import pickle
 
 from tqdm import tqdm
 
-from config import IMG_DIR
+from config import IMG_DIR, pickle_file
 
 
 def get_data():
     samples = []
-    dirs = [os.path.join(IMG_DIR, d) for d in os.listdir(IMG_DIR) if os.path.isdir(os.path.join(IMG_DIR, d))]
-    for dir in tqdm(dirs):
+    dirs = [d for d in os.listdir(IMG_DIR) if os.path.isdir(os.path.join(IMG_DIR, d))]
+    for d in tqdm(dirs):
+        dir = os.path.join(IMG_DIR, d)
         files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
 
         for f in files:
             img_path = os.path.join(dir, f)
-            samples.append(img_path)
+            img_path = img_path.replace('\\', '/')
+            samples.append({'img': img_path, 'label': d})
 
     return samples
 
@@ -24,11 +27,19 @@ if __name__ == "__main__":
     num_not_same = num_total - num_same
 
     samples = get_data()
+    with open(pickle_file, 'wb') as file:
+        pickle.dump(samples, file)
+
     print(len(samples))
     print(samples[:10])
+
+    out_lines = []
 
     for _ in range(num_same):
         None
 
     for _ in range(num_not_same):
         None
+
+    with open('data/test_pairs.txt', 'w') as file:
+        file.writelines(out_lines)
