6a87a3a7db03d286faafcb830dfb7b3480948e24
Yang Liu <liuyang12@focusmedia.cn>
2019-04-26 10:12:40 +0800

    update

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
+print(device)
 
 # Model parameters
 im_size = 112
