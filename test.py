83ff6134260c2de0bb9797ffe6868c8d07c95691
Yang Liu <liuyang12@focusmedia.cn>
2019-05-16 10:17:08 +0800

    update

--- test.py
+++ test.py
@@ -190,6 +190,7 @@ def error_analysis(threshold):
 
 def copy_file(old, new):
     old = os.path.join(IMG_FOLDER, old)
+    print(old)
     img = cv.imread(old)
     new_fn = os.path.join('images', new)
     cv.imwrite(new_fn, img)
