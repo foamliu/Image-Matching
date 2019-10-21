2c9f5d582effe93ce0c4147a28df4551d25dd366
Yang Liu <liuyang12@focusmedia.cn>
2019-10-21 17:27:39 +0800

    update

--- export.py
+++ export.py
@@ -12,9 +12,10 @@ if __name__ == '__main__':
     checkpoint = torch.load(checkpoint)
     print('elapsed {} sec'.format(time.time() - start))
     model = checkpoint['model'].module
-    metric_fc = checkpoint['metric_fc']
+    metric_fc = checkpoint['metric_fc'].module
 
     filename = 'image_matching_mobile.pt'
+    print('saving {}...'.format(filename))
     start = time.time()
     torch.save(model.state_dict(), filename)
     print('elapsed {} sec'.format(time.time() - start))
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     print('elapsed {} sec'.format(time.time() - start))
 
     filename = 'metric_fc.pt'
+    print('saving {}...'.format(filename))
     start = time.time()
     torch.save(metric_fc.state_dict(), filename)
     print('elapsed {} sec'.format(time.time() - start))
