372efb4136389e845379e547cdfd7aa3df726506
Yang Liu <liuyang12@focusmedia.cn>
2019-10-30 09:10:35 +0800

    update

--- test_531.py
+++ test_531.py
@@ -119,6 +119,7 @@ def evaluate(model):
 
 
 def get_threshold():
+    print('get threshold...')
     # return 25.50393648495902
     with open(angles_file, 'r') as file:
         lines = file.readlines()
@@ -134,7 +135,7 @@ def get_threshold():
     min_error = 6000
     min_threshold = 0
 
-    for d in data:
+    for d in tqdm(data):
         threshold = d['angle']
         type1 = len([s for s in data if s['angle'] <= threshold and s['type'] == 0])
         type2 = len([s for s in data if s['angle'] > threshold and s['type'] == 1])
@@ -206,14 +207,15 @@ def visualize(threshold):
 
 def test(model):
     # print('Generating features...')
-    gen_features(model)
+    # gen_features(model)
 
     # print('Evaluating {}...'.format(angles_file))
-    evaluate(model)
+    # evaluate(model)
 
     # print('Calculating threshold...')
     # threshold = 70.36
     thres = get_threshold()
+    print(thres)
     # print('Calculating accuracy...')
     acc = accuracy(thres)
     print('Accuracy: {}'.format(acc * 100))
@@ -222,8 +224,8 @@ def test(model):
 
 
 if __name__ == "__main__":
-    if not os.path.isdir('data/data'):
-        extract('data/jinhai_531.tar.gz')
+    # if not os.path.isdir('data/data'):
+    #     extract('data/jinhai_531.tar.gz')
 
     # checkpoint = 'BEST_checkpoint.tar'
     # checkpoint = torch.load(checkpoint)
