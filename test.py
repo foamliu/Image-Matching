ebac93943d4fc8c1aff73f1ebfc7cbdb878ea43a
Yang Liu <liuyang12@focusmedia.cn>
2019-11-01 15:17:36 +0800

    udpate

--- test.py
+++ test.py
@@ -263,21 +263,19 @@ if __name__ == "__main__":
     device = torch.device('cpu')
     print('test with {}'.format(device))
 
-    # checkpoint = 'BEST_checkpoint.tar'
-    # checkpoint = torch.load(checkpoint, map_location=torch.device('cpu'))
-    # model = checkpoint['model'].module
+    checkpoint = 'BEST_checkpoint.tar'
+    checkpoint = torch.load(checkpoint, map_location=torch.device('cpu'))
+    model = checkpoint['model'].module
+
+    # filename = 'image_matching_mobile.pt'
+    # model = MobileNetV2()
+    # model.load_state_dict(torch.load(filename))
 
     # scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
     # model = torch.jit.load(scripted_quantized_model_file)
 
     # scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
     # model = torch.jit.load(scripted_float_model_file)
-
-    from mobilenet_v2 import MobileNetV2
-
-    filename = 'image_matching_mobile.pt'
-    model = MobileNetV2()
-    model.load_state_dict(torch.load(filename))
 
     model = model.to(device)
     model.eval()
