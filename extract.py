bb2c3abf9b5a37898325ac541e8c4752b5b84104
unknown <foamliu@yeah.net>
2019-11-20 20:29:19 +0800

    update

--- extract.py
+++ extract.py
@@ -3,3 +3,4 @@ from utils import extract
 if __name__ == "__main__":
     # if not os.path.isdir('data/cron20190326_resized'):
     extract('data/cron20190326.zip')
+    extract('data/cron20190415.zip')
