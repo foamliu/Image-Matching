6b554c8542170a7d32973590493c711fbf1177cd
Yang Liu <liuyang12@focusmedia.cn>
2019-11-21 14:28:54 +0800

    update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets de
 # Model parameters
 im_size = 224
 channel = 3
-emb_size = 512
+emb_size = 128
 
 # Training parameters
 num_workers = 1  # for data-loading; right now, only 1 works with h5py
