a8c46835c721d37875ad1edcace5493020954564
Yang Liu <liuyang12@focusmedia.cn>
2019-11-30 07:36:35 +0800

    update

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import warnings
 import numpy as np
 import torch
 from torch import nn
-from torch.optim.lr_scheduler import MultiStepLR
+# from torch.optim.lr_scheduler import MultiStepLR
 from torch.utils.tensorboard import SummaryWriter
 
 from config import device, grad_clip, print_freq, num_workers
@@ -60,7 +60,7 @@ def train_net(args):
                                                num_workers=num_workers)
 
     # scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30, 40], gamma=0.1)
-    scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30], gamma=0.1)
+    # scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30], gamma=0.1)
 
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
@@ -85,7 +85,7 @@ def train_net(args):
         writer.add_scalar('model/valid_accuracy', val_acc, epoch)
         writer.add_scalar('model/valid_threshold', thres, epoch)
 
-        scheduler.step(epoch)
+        # scheduler.step(epoch)
 
         # Check if there was an improvement
         is_best = val_acc > best_acc
