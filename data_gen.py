f1855b25e7cadcfe558fa9cc07f087c5888d01ac
Yang Liu <liuyang12@focusmedia.cn>
2019-12-02 18:11:01 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -21,7 +21,7 @@ data_transforms = {
     ]),
     'val': transforms.Compose([
         # transforms.Resize(256),
-        # transforms.CenterCrop(224),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
