c7f135043cb9f2196530c459c24982681a28b510
Yang Liu <liuyang12@focusmedia.cn>
2019-12-10 09:50:28 +0800

    update

--- export.py
+++ export.py
@@ -2,8 +2,7 @@ import time
 
 import torch
 
-from mobilenet_v2 import MobileNetV2
-from models import ArcMarginModel
+from models import ResNetMatchModel
 
 if __name__ == '__main__':
     checkpoint = 'BEST_checkpoint.tar'
@@ -14,28 +13,25 @@ if __name__ == '__main__':
     model = checkpoint['model'].module
     metric_fc = checkpoint['metric_fc'].module
 
-    filename = 'image_matching_mobile.pt'
+    filename = 'image_matching.pt'
     print('saving {}...'.format(filename))
     start = time.time()
     torch.save(model.state_dict(), filename)
     print('elapsed {} sec'.format(time.time() - start))
 
-    # print(model)
-
     print('loading {}...'.format(filename))
     start = time.time()
-    model = MobileNetV2()
+    model = ResNetMatchModel()
     model.load_state_dict(torch.load(filename))
     print('elapsed {} sec'.format(time.time() - start))
 
-    # filename = 'metric_fc.pt'
-    # print('saving {}...'.format(filename))
-    # start = time.time()
-    # torch.save(metric_fc.state_dict(), filename)
-    # print('elapsed {} sec'.format(time.time() - start))
-    #
-    # print('loading {}...'.format(filename))
-    # start = time.time()
-    # metric_fc = ArcMarginModel()
-    # metric_fc.load_state_dict(torch.load(filename))
-    # print('elapsed {} sec'.format(time.time() - start))
+    filename_scripted = 'image_matching_scripted.pt'
+    print('saving {}...'.format(filename_scripted))
+    start = time.time()
+    torch.jit.save(torch.jit.script(model), filename_scripted)
+    print('elapsed {} sec'.format(time.time() - start))
+
+    print('loading {}...'.format(filename))
+    start = time.time()
+    model = torch.jit.load(filename_scripted)
+    print('elapsed {} sec'.format(time.time() - start))
