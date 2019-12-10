5251d7c49db03b07c07acf8e790089ddebd02a7e
Yang Liu <liuyang12@focusmedia.cn>
2019-12-10 09:38:20 +0800

    update

--- test.py
+++ test.py
@@ -284,7 +284,8 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    # device = torch.device('cpu')
+    if not args.gpu:
+        device = torch.device('cpu')
     print('test with {}'.format(device))
 
     checkpoint = 'BEST_checkpoint.tar'
