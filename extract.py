d45f2b615bd935b9d400e90616f193585f1da384
Yang Liu <liuyang12@focusmedia.cn>
2020-01-16 15:55:01 +0800

    update

--- extract.py
+++ extract.py
@@ -3,9 +3,8 @@ import os
 from utils import extract_zip, extract_tgz
 
 if __name__ == "__main__":
-    if not os.path.isdir('data/cron20190326'):
+    if not os.path.isdir('data/data/frame/cron20190326/'):
         extract_zip('data/cron20190326.zip')
-    if not os.path.isdir('data/cron20190326'):
         extract_zip('data/cron20190415.zip')
 
     extract_tgz('data/jinhai_531.tar.gz')
