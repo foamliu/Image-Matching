be68637a6568a4624b690afe70e40b75c9b296ff
Yang Liu <liuyang12@focusmedia.cn>
2020-01-16 15:24:41 +0800

    update

--- preprocess_train.py
+++ preprocess_train.py
@@ -1,5 +1,7 @@
 import os
 
+from tqdm import tqdm
+
 from config import IMG_DIR
 
 
@@ -8,8 +10,30 @@ def get_data():
     dirs = [d for d in os.listdir(IMG_DIR) if os.path.isdir(os.path.join(IMG_DIR, d))]
     print('len(dirs): ' + str(len(dirs)))
 
+    for d in tqdm(dirs):
+        build_mapping(d)
+
+        dir = os.path.join(IMG_DIR, d)
+        files = [f for f in os.listdir(dir) if f.endswith('.jpg')]
+
+        for f in files:
+            img_path = os.path.join(d, f)
+            samples.append({'img_path': img_path, 'label': dir2label[d]})
+
     return samples
 
 
+def build_mapping(dir):
+    global dir2label, label2dir
+    if not dir in dir2label:
+        next_index = len(dir2label)
+        dir2label[dir] = next_index
+        label2dir[next_index] = dir
+
+
 if __name__ == "__main__":
-    get_data()
+    dir2label = {}
+    label2dir = {}
+
+    samples = get_data()
+    print('len(samples): ' + str(len(samples)))
